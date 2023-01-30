3eb969f17f1756a43c749edbe5d19f819e61d4e3
malomuzh-bo <malomuzh.bo@gmail.com>
2023-01-30 21:47:59 +0200

    commit

---
+++ Lab3001/todolist/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todolist';
+  quote1 = "Patience is a key element of success";
+  quote2 = "Success is a lousy teacher. It seduces smart people into thinking they can’t lose";
+  quote3 = "If you think your teacher is tough, wait till you get a boss";
+  quote4 = "Be nice to nerds. Chances are you’ll end up working for one";
+  quote5 = "Life is not fair — get used to it!";
+}
